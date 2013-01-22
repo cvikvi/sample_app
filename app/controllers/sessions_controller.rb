9de899930b4cdea55c7fcf9b0e59dfc9a1f9b7df
Willie Green <green_ww@tsu.edu>
2013-01-22 17:04:26 -0600

    cucumber tryout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ def new
   	user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
@@ -14,6 +16,8 @@ def new
   end
 
   def destroy
+  	sign_out
+  	redirect_to root_url
   end
 
 end
